bd5f6e06d9625910f8db871d9ad316b9a3497abb
antfromano <antfromano@gmail.com>
2021-09-28 17:44:15 +0000

    updating app.py to address 500 internal server error on update.slice.html page

--- app.py
+++ app.py
@@ -77,5 +77,5 @@ def update_slice(complete_slices_id):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=False)
+            debug=True)
 
